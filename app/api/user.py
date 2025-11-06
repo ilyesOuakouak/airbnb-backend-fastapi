5170788e8585cf5d70444fedeba6826e48d027fc
ouakouak.ilyes@gmail.com <ouakouak.ilyes@gmail.com>
2025-11-06 15:12:11 +0100

    feat(users): add GET /users/{id} endpoint with error handling and response schema

--- app/api/user.py
+++ app/api/user.py
@@ -41,6 +41,15 @@ def register(user: UserCreate, db: Session = Depends(get_db)):
 
     return {"id": new_user.id, "email": new_user.email}
 
+@router.get("/{user_id}", response_model=UserResponse, status_code=status.HTTP_200_OK)
+def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found !")
+
+    return user
+
 @router.get("/list", response_model=List[UserResponse],  status_code=status.HTTP_200_OK)
 def get_users(db: Session = Depends(get_db)):
     users = db.query(User).all()
